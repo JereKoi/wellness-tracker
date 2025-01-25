674be84e20169845723e1847abd9cf5c71fa4d93
Jere <jerekoivisto7@gmail.com>
2025-01-25 12:04:33 +0200

    Started some initial form styling.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,29 @@
-import Form from 'next/form'
- 
+import Form from "next/form";
+
 export default function Page() {
   return (
-    <Form action="/search">
+    <Form action="/submit">
       {/* On submission, the input value will be appended to 
           the URL, e.g. /search?query=abc */}
-      <input name="query" />
-      <button className="submit-btn" type="submit">Submit</button>
+      <h1 className="color:blue">
+        <strong>Wellness-tracker</strong>
+      </h1>
+      <div className="grid-main grid gap-2">
+        <input className="placeholder-gray-500 border" placeholder="Physical activity in hours today in total (ex. 2h)" />
+        <br></br>
+        <input className="placeholder-gray-500 border" placeholder="Sleep in hours last night in total (ex. 8h)" />
+        <br></br>
+        <input className="placeholder-gray-500 border" placeholder="Water intake today in total (ex. 2l)" />
+        <br></br>
+        <input className="placeholder-gray-500 border" placeholder="(optional) Calories today in total (ex. 2000kcal)" />
+        <br></br>
+        <button
+          className="bg-violet-500 hover:bg-violet-600 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-violet-700 ..."
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
     </Form>
-  )
+  );
 }
