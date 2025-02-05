a91db069f1b8689621d10de39f22cf780a2fbc11
Jere <jerekoivisto7@gmail.com>
2025-02-05 15:28:03 +0200

    Working on login, register forms and submitting the info user enters to backend.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,10 +9,21 @@ const RegisterPage = () => {
   })
   const [pending, setPending] = useState(false)
 
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setPending(true);
+
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json"},
+      body: JSON.stringify(form),
+    });
+  }
+
   return (
     <>
       <div className="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" action="#">
+        <form onSubmit={handleSubmit} className="space-y-6" action="#">
           <h5 className="text-xl font-medium text-gray-900 dark:text-white">
             Register to our platform
           </h5>
@@ -45,6 +56,9 @@ const RegisterPage = () => {
             <input
               type="email"
               name="email"
+              disabled={pending}
+              value={form.email}
+              onChange={(e) => setForm({...form, email:e.target.value})}
               id="email"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               placeholder="name@company.com"
@@ -61,7 +75,10 @@ const RegisterPage = () => {
             <input
               type="password"
               name="password"
+              disabled={pending}
               id="password"
+              value={form.password}
+              onChange={(e) => setForm({...form, password:e.target.value})}
               placeholder="••••••••"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
@@ -77,7 +94,10 @@ const RegisterPage = () => {
             <input
               type="password"
               name="password"
+              disabled={pending}
               id="password"
+              value={form.confirmPassword}
+              onChange={(e) => setForm({...form, confirmPassword:e.target.value})}
               placeholder="••••••••"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
               required
@@ -85,6 +105,7 @@ const RegisterPage = () => {
           </div>
           <button
             type="submit"
+            disabled={pending}
             className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Register
